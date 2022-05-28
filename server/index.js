f280ca936d3811cd42514e6d621f818ad2e0bff4
mariapopadiuk <mpopadiuk13@gmail.com>
2022-05-28 17:33:18 +0100

    database created, connected, tested

--- server/index.js
+++ server/index.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/api/blogs', (req, res) => {
+app.get('/api', (req, res) => {
   res.send("[{ title: '123', tex: 'sex', id: 1 }, { title: '123', tex: 'sex', id: 2 }, { title: '123', tex: 'sex', id: 3 }]")
 })
-app.get('/api/coments', (req, res) => {
-  res.send("[{ title: 'ffgsfg', tex: 'sex', id: 1 }, { title: 'sgfdgdf', tex: 'sex', id: 2 }, { title: 'dgf', tex: 'sex', id: 3 }]")
-})
+
 app.listen(port, () => {
   console.log(`Example   app listening on port ${port}`)
 })
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'blog_etic'
+});
+
+connection.connect();
+
+connection.query('select * from users', function (error, results, fields) {
+  if (error) throw error;
+  console.log('users sao: ', results);
+});
+
+connection.end();
