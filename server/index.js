ea425395a06ec163652bf141a03dacd6d81dfba4
mariapopadiuk <mpopadiuk13@gmail.com>
2022-05-28 17:50:05 +0100

    added dotenv

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const port = 4000
 
 app.get('/api', (req, res) => {
@@ -12,10 +13,10 @@ app.listen(port, () => {
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'blog_etic'
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME
 });
 
 connection.connect();
