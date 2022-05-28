e9fc4976f8116e39f4ef7b267c058d9fdcb7ecc8
mariapopadiuk <mpopadiuk13@gmail.com>
2022-05-28 20:23:44 +0100

    added CRUD routes

--- server/index.js
+++ server/index.js
@@ -3,15 +3,15 @@ const app = express()
 require('dotenv').config()
 const bcrypt = require('bcrypt')
 const port = 4000
+const bodyParser = require("body-parser");
 
-app.get('/api', (req, res) => {
-  res.send("hello world")
-})
-//databese connection
 app.listen(port, () => {
   console.log(`Example   app listening on port ${port}`)
 })
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+//databese connection
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
@@ -22,9 +22,53 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('select * from users', function (error, result, fields) {
-  if (error) throw error;
-  console.log(result);
-});
+app.get('/api/posts', (req, res) => {
 
-connection.end();
+  connection.query('select * from posts', function (error, result, fields) {
+    if (error) throw error;
+    res.send(result)
+  });
+  // connection.end();
+})
+
+//add post route
+app.post('/api/add-post', (req, res) => {
+  const { user_id, title, text, date_created, img_url} = req.body;
+  connection.query(`insert into posts (user_id, title, text, date_created, img_url) values (${user_id}, '${title}', '${text}', '${date_created}', '${img_url}');` , function (error, result, fields) {
+    if (error) {
+      res.end('error');
+      throw error;
+    }
+  });
+  res.end('post added');
+  // connection.end();
+})
+
+
+//update post route
+app.put('/api/put-post/:id', (req, res) => {
+  const id = req.params.id
+  console.log(id);
+  const { user_id, title, text, date_created, img_url } = req.body;
+  connection.query(`update posts SET title="${title}", text="${text}", date_created="${date_created}", img_url="${img_url}" WHERE post_id=${id};`, function (error, result, fields) {
+    if (error) { 
+      res.end('error');
+      throw error;
+    }
+  });
+  res.end('post updated');
+  // connection.end();
+})
+
+//delete post route
+app.delete('/api/delete-post/:id', (req, res) => {
+  const id = req.params.id
+  connection.query(`delete from posts WHERE post_id=${id};`, function (error, result, fields) {
+    if (error) {
+      res.end('error');
+      throw error;
+    }
+  });
+  res.end('post deleted');
+  // connection.end();
+})
