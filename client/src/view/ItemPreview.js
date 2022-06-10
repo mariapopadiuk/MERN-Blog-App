cb462499874804abcf3184ecd3dcd0e7ac8589bc
mariapopadiuk <mpopadiuk13@gmail.com>
2022-06-10 18:12:32 +0100

    got item by id from server

--- client/src/view/ItemPreview.js
+++ client/src/view/ItemPreview.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import Footer from '../Components/Footer'
 import Header from '../Components/Header'
 import { useParams } from "react-router-dom";
@@ -9,31 +9,28 @@ import img from '../img/bgitem.jpg'
 import { FaCheck, FaEdit } from 'react-icons/fa'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Context } from '../context/context';
+import axios from 'axios';
 
 
 
 export default function ItemPreview() {
   let { id } = useParams();
-
-  const [isAdmin, setIsAdmin] = useState(false);
+  const { isAuthenticated} = useContext(Context);  
   const [titleEditable, setTitleEditable] = useState(false);
   const [textEditable, setTextEditable] = useState(false);
-  
 
-  
-// Form Validation
+// Form Validation for comment
   const [formValue, setformValue] = useState({
     username: '',
     text: '',
   })
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(123);
     if (formValue.username === '') {
       toast.error("Please enter your Name!");
       return false
     }
-
     if (formValue.text === '') {
       toast.error("Please enter you message!");
 
@@ -48,7 +45,6 @@ export default function ItemPreview() {
     toast.success("Comment added! Thank you");
   }
 
-
   const handleChange = (event) => {
     // console.log(2345);
     setformValue({
@@ -58,8 +54,13 @@ export default function ItemPreview() {
   }
 
 
-
-
+  
+  const [data, setData] = useState(null);
+  useEffect(() => {
+  axios.get(`http://localhost:4000/posts/${id}`).then((res) => setData(res.data[0]))
+  }, []);
+  console.log(data);
+   
 
   return (
     <>
@@ -69,14 +70,14 @@ export default function ItemPreview() {
       <section className='container font-serif mx-auto bg-bege tracking-wide'>
 
         <div>
-          <img src={img} className="w-3/4 m-auto p-10" />
+          <img src={data?.img_url} className="w-3/4 m-auto p-10" />
         </div>
 
         <h1 className={(titleEditable && 'border') + ' border-red-600 text-green text-center text-4xl pb-5 font-semibold'} contentEditable={titleEditable} >
-          {data[id].title}&nbsp;
+          {data?.title}&nbsp;
 
           {
-            isAdmin &&
+            isAuthenticated &&
             (titleEditable
               ? <FaCheck onClick={() => setTitleEditable(false)} className='m-auto inline text-gold hover:text-green transition cursor-pointer' />
               : <FaEdit onClick={() => setTitleEditable(true)} className='m-auto inline text-gold hover:text-red-600 transition cursor-pointer' />
@@ -86,19 +87,20 @@ export default function ItemPreview() {
 
 
         <p className='text-center font-bold pb-5 text-gold drop-shadow-lg'>
-          {data[id].date}
+          {data?.date}
         </p>
 
         <p className={(textEditable && 'border') + ' border-red-600 p-3 container mx-auto w-3/4 text-green text-center pb-10'} focus="true" contentEditable={textEditable}  >
-          {data[id].text}
+          {data?.text}
           {
-            isAdmin &&
+            isAuthenticated &&
             (textEditable
               ? <FaCheck onClick={() => setTextEditable(false)} className='m-auto text-4xl text-gold hover:text-green transition cursor-pointer' />
               : <FaEdit onClick={() => setTextEditable(true)} className='m-auto text-4xl text-gold hover:text-red-600 transition cursor-pointer' />
             )
           }
         </p>
+           
 
         <p className='ml-40 text-left font-bold text-xl pb-5 '>
           Comments
@@ -106,7 +108,8 @@ export default function ItemPreview() {
         {
           comments.map((c, i) => {
             if (c.postID === +id) {
-              return <Comment isadmin={ isAdmin}  name={c.name} id={c.id} date={c.date} text={c.text} postID={c.postID} key={i} />
+              return <Comment isadmin={isAuthenticated
+}  name={c.name} id={c.id} date={c.date} text={c.text} postID={c.postID} key={i} />
             }
           }
           )
