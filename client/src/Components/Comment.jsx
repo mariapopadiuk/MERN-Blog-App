1d183bae8917afb236f9b74df6d8544fd7f51887
mariapopadiuk <mpopadiuk13@gmail.com>
2022-06-11 20:19:05 +0100

    real time comment deletion proccess

--- client/src/Components/Comment.jsx
+++ client/src/Components/Comment.jsx
@@ -1,43 +1,31 @@
 import React, { useContext } from 'react'
 import { FaTrash } from 'react-icons/fa'
 import { Context } from '../context/context';
-import axios from 'axios';
 import moment from 'moment'
 
-export default function Comment({ id, name, date, text }) { 
+export default function Comment({ handleDeleteComment, name, date, text, id }) { 
 
-  const { isAuthenticated, token } = useContext(Context);
-  function deleteComment() {
-    axios({
-      method: 'delete', //you can set what request you want to be
-      url: 'http://localhost:4000/comments/delete-comment/',
-      data: { comment_id: id },
-      headers: {
-        Authorization: 'Bearer ' + token
-      }
-    }) 
-  } 
- 
+  const { isAuthenticated} = useContext(Context);
   return (
 
     <div className=' container font-serif text-green ml-40 pb-5'>
-      <p className=' font-semibold'>
-        <h3 className='text-xl'>{name}</h3>
+      <div className=' font-semibold'>
+        <p className='text-xl'>{name}</p>
         {moment(date).format('lll', 'pt')
 }
-      </p>
-      <p className='w-2/4 pl-3 p-2 shadow-md text-green flex justify-between'>
-        <div className='min-w-4/6 '>
+      </div>
+      <div className='w-2/4 pl-3 p-2 shadow-md text-green flex justify-between'>
+        <p className='min-w-4/6 '>
           {text}
-        </div>
+        </p>
         <div className='flex justify-between'>
           {
             isAuthenticated
             &&
-            <FaTrash onClick={deleteComment} className='mr-5 text-gold hover:text-red-600 transition cursor-pointer' />
+            <FaTrash onClick={() => handleDeleteComment(id)} className='mr-5 text-gold hover:text-red-600 transition cursor-pointer' />
           }
         </div>
-      </p>
+      </div>
     </div>
   )
 }
--- client/src/view/ItemPreview.js
+++ client/src/view/ItemPreview.js
@@ -10,16 +10,17 @@ import { Context } from '../context/context';
 import axios from 'axios';
 import moment from 'moment'
 
-
 export default function ItemPreview() {
   let { id } = useParams();
-  const { isAuthenticated } = useContext(Context);
+  const { isAuthenticated, token } = useContext(Context);
   const [titleEditable, setTitleEditable] = useState(false);
   const [textEditable, setTextEditable] = useState(false);
   const [data, setData] = useState(null);
   const [comments, setComments] = useState(null);
   const [whenToUpdate, setwhenToUpdate] = useState(0);
-
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  };
   // Form Validation for comment
   const [formValue, setformValue] = useState({
     user_name: '',
@@ -37,15 +38,15 @@ export default function ItemPreview() {
       toast.error("Please enter you message!");
       return false
     }
-
+    //ADD COMMENT
     axios.post(`http://localhost:4000/comments/add-comment/${id}`, formValue)
       .then(function () {
-
         setformValue({
           user_name: '',
           text: '',
         })
-        toast.success("Comment added!"); 
+        setwhenToUpdate(whenToUpdate + 1)
+        toast.success("Comment added!");
       })
       .catch(function (error) {
         toast.error(error.message);
@@ -60,69 +61,114 @@ export default function ItemPreview() {
       date_created: new Date().toISOString().slice(0, 19).replace('T', ' '),
       post_id: id
     });
-    console.log(formValue);
+  }
+  // UPDATE POST
+  function updatePost() {
+    axios.put(`http://localhost:4000/posts/update-post/${id}`,
+      {
+        data: {
+          "user_id": "2",
+          "title": "Updated now postmen",
+          "text": "Flor from postmen",
+          "date_created": "2022/08/12",
+          "img_url": "http://localhost:3000/static/media/bgitem.fbd33972cddaee0db00a.jpg"
+        }, headers
+      }).then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setwhenToUpdate(whenToUpdate + 1)
+          toast.success("Post updated");
+          setTextEditable(false)
+        } else {
+          toast.error("Something went wrong!");
+        }
+      })
   }
 
+  // DELETE COMMENT
+  function deleteComment(comentId) {
+    axios.delete('http://localhost:4000/comments/delete-comment/',
+      {
+        data: { comment_id: comentId },
+        headers
+      }).then((res) => {
+        if (res.status === 200) {
+          setwhenToUpdate(whenToUpdate + 1)
+          toast.success("Comment deleted");
+        } else {
+          toast.error("Something went wrong!");
+        }
+      })
+  }
+
+  // GET POST
   useEffect(() => {
-    axios.get(`http://localhost:4000/posts/${id}`).then((res) => setData(res.data[0])); 
-  }, []); 
- 
+    axios.get(`http://localhost:4000/posts/${id}`).then((res) => setData(res.data[0]));
+  }, []);
+
+  // GET COMMENTS
   useEffect(() => {
-    axios.get(`http://localhost:4000/comments/${id}`).then((res) => setComments(res.data)); 
-  }, [formValue]); 
+    axios.get(`http://localhost:4000/comments/${id}`).then((res) => setComments(res.data));
+  }, [whenToUpdate]);
 
   return (
     <>
       <Header />
       <ToastContainer position="top-left" />
-
       <section className='container font-serif mx-auto bg-bege tracking-wide'>
-
         <div>
           <img src={data?.img_url} className="w-3/4 m-auto p-10" />
         </div>
-
         <h1 className={(titleEditable && 'border') + ' border-red-600 text-green text-center text-4xl pb-5 font-semibold'} contentEditable={titleEditable} >
           {data?.title}&nbsp;
-
           {
             isAuthenticated &&
             (titleEditable
-              ? <FaCheck onClick={() => setTitleEditable(false)} className='m-auto inline text-gold hover:text-green transition cursor-pointer' />
-              : <FaEdit onClick={() => setTitleEditable(true)} className='m-auto inline text-gold hover:text-red-600 transition cursor-pointer' />
+              ? <FaCheck
+                onClick={updatePost}
+                className='m-auto inline text-gold hover:text-green transition cursor-pointer'
+              />
+              : <FaEdit
+                onClick={() => setTitleEditable(true)}
+                className='m-auto inline text-gold hover:text-red-600 transition cursor-pointer'
+              />
             )
           }
         </h1>
-
-
         <p className='text-center font-bold pb-5 text-gold drop-shadow-lg'>
           {moment(data?.date).format('lll', 'pt')}
         </p>
-
-        <p className={(textEditable && 'border') + ' border-red-600 p-3 container mx-auto w-3/4 text-green text-center pb-10'} focus="true" contentEditable={textEditable}  >
+        <p
+          className={(textEditable && 'border') + ' border-red-600 p-3 container mx-auto w-3/4 text-green text-center pb-10'}
+          focus="true"
+          contentEditable={textEditable}
+        >
           {data?.text}
           {
             isAuthenticated &&
             (textEditable
-              ? <FaCheck onClick={() => setTextEditable(false)} className='m-auto text-4xl text-gold hover:text-green transition cursor-pointer' />
-              : <FaEdit onClick={() => setTextEditable(true)} className='m-auto text-4xl text-gold hover:text-red-600 transition cursor-pointer' />
+              ? <FaCheck
+                onClick={() => updatePost}
+                className='m-auto text-4xl text-gold hover:text-green transition cursor-pointer'
+              />
+              : <FaEdit
+                onClick={() => setTextEditable(true)}
+                className='m-auto text-4xl text-gold hover:text-red-600 transition cursor-pointer'
+              />
             )
           }
         </p>
-
-
         <p className='ml-40 text-left font-bold text-xl pb-5 '>
           Comments
         </p>
         {
           comments?.map((c, i) => (
 
-            <Comment useEffDep={formValue} name={c.user_name} id={c.comment_id} date={c.date_created} text={c.text} postID={c.postID} key={i} />
+            <Comment useEffDep={formValue} name={c.user_name} id={c.comment_id} date={c.date_created} text={c.text} postID={c.postID} key={i} handleDeleteComment={deleteComment} />
 
           )
           )
         }
-
         <p className='ml-40 text-left font-bold text-xl'>
           Add comments
         </p>
@@ -140,12 +186,7 @@ export default function ItemPreview() {
             </button>
           </div>
         </form>
-
-
-
       </section>
-
-
       <Footer />
     </>
   )
